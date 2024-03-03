cf57bbae18786126cad78f067bd3b5b82d8f4e1c
Pascal <152603632+K-Pascal@users.noreply.github.com>
2024-03-03 04:54:09 +0100

    free the code from 42Norm

--- get_next_line.c
+++ get_next_line.c
@@ -6,34 +6,49 @@
 /*   By: pnguyen- <pnguyen-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/21 11:36:32 by pnguyen-          #+#    #+#             */
-/*   Updated: 2024/03/03 04:39:29 by pnguyen-         ###   ########.fr       */
+/*   Updated: 2024/03/03 05:04:26 by pnguyen-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 #include <stdlib.h>
 
-#include "get_next_line.h"
+#ifndef BUFFER_SIZE
+# define BUFFER_SIZE 64
+#endif
+
+#if BUFFER_SIZE < 0
+# undef BUFFER_SIZE
+# define BUFFER_SIZE 0
+#endif
+
+typedef struct s_str
+{
+	char	*data;
+	size_t	len;
+	size_t	size;
+}	t_str;
 
 static int		gnl_absorb_buffer(t_str *str, char buffer[BUFFER_SIZE]);
-static ssize_t	gnl_read(t_str *str, int fd, char buffer[BUFFER_SIZE]);
-static char		*gnl_str_realloc(char src[], size_t len, size_t size);
+static ssize_t	gnl_read(t_str *str, int const fd, char buffer[BUFFER_SIZE]);
+static char		*gnl_str_realloc(char src[], size_t const len, size_t const size);
+static void		ft_strlcpy(char dest[], char const src[], size_t size);
+static char		*ft_strchr(char const str[], char const c);
 
 char	*get_next_line(int fd)
 {
 	static char	buffer[BUFFER_SIZE];
-	ssize_t		bytes;
-	t_str		line;
-
 	if (fd < 0 || BUFFER_SIZE == 0)
 		return (NULL);
+
+	t_str		line;
 	line.size = BUFFER_SIZE + 1;
 	line.data = malloc(line.size * sizeof(char));
 	if (line.data == NULL)
 		return (NULL);
 	if (gnl_absorb_buffer(&line, buffer))
 		return (line.data);
-	bytes = gnl_read(&line, fd, buffer);
+	ssize_t const	bytes = gnl_read(&line, fd, buffer);
 	if (bytes == -1 || line.len == 0)
 	{
 		free(line.data);
@@ -44,9 +59,7 @@ char	*get_next_line(int fd)
 
 static int	gnl_absorb_buffer(t_str *str, char buffer[BUFFER_SIZE])
 {
-	char	*stash;
-
-	stash = ft_strchr(buffer, '\n');
+	char	*stash = ft_strchr(buffer, '\n');
 	if (stash == NULL)
 	{
 		str->len = ft_strchr(buffer, '\0') - buffer;
@@ -64,12 +77,10 @@ static int	gnl_absorb_buffer(t_str *str, char buffer[BUFFER_SIZE])
 	}
 }
 
-static ssize_t	gnl_read(t_str *str, int fd, char buffer[BUFFER_SIZE])
+static ssize_t	gnl_read(t_str *str, int const fd, char buffer[BUFFER_SIZE])
 {
-	char	*stash;
-	ssize_t	bytes;
-
-	stash = NULL;
+	ssize_t	bytes = -1;
+	char	*stash = NULL;
 	while (stash == NULL)
 	{
 		if (str->size <= str->len + BUFFER_SIZE)
@@ -91,13 +102,31 @@ static ssize_t	gnl_read(t_str *str, int fd, char buffer[BUFFER_SIZE])
 	return (bytes);
 }
 
-static char	*gnl_str_realloc(char src[], size_t len, size_t size)
+static char	*gnl_str_realloc(char src[], size_t const len, size_t const size)
 {
-	char	*new_str;
-
-	new_str = malloc(size * sizeof(char));
+	char	*new_str = malloc(size * sizeof(char));
 	if (new_str != NULL)
 		ft_strlcpy(new_str, src, len + 1);
 	free(src);
 	return (new_str);
+}
+
+static void	ft_strlcpy(char dest[], char const src[], size_t size)
+{
+	if (size == 0)
+		return ;
+	while (--size != 0 && *src != '\0')
+		*(dest++) = *(src++);
+	*dest = '\0';
+}
+
+static char	*ft_strchr(char const str[], char const c)
+{
+	while (*str != c)
+	{
+		if (*str == '\0')
+			return (0);
+		++str;
+	}
+	return ((char *)str);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,34 +6,13 @@
 /*   By: pnguyen- <pnguyen-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/21 16:36:56 by pnguyen-          #+#    #+#             */
-/*   Updated: 2024/02/10 12:04:21 by pnguyen-         ###   ########.fr       */
+/*   Updated: 2024/03/03 05:01:33 by pnguyen-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-# ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 64
-# endif
-
-# if BUFFER_SIZE < 0
-#  undef BUFFER_SIZE
-#  define BUFFER_SIZE 0
-# endif
-
-# include <stddef.h>
-
-typedef struct s_str
-{
-	char	*data;
-	size_t	len;
-	size_t	size;
-}	t_str;
-
 char	*get_next_line(int fd);
-
-void	ft_strlcpy(char dest[], char const src[], size_t size);
-char	*ft_strchr(char str[], char c);
 
 #endif
